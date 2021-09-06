441c1c328f00af669e38998258c3ea84a7e264bf
Francisco <franperc@gmail.com>
2021-09-05 22:39:22 -0400

    urls settings

--- apiproject/urls.py
+++ apiproject/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.conf.urls import url, include
 from django.urls.conf import path
 from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('apiapp/',include('apiapp.urls'))
